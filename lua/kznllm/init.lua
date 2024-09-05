e20d7432b4f744f8da3c0c84c189394659b8e5cb
chottolabs <171991982+chottolabs@users.noreply.github.com>
2024-09-05 11:04:25 -0400

    use noop call before writing content * avoids undojoin after undo error

--- lua/kznllm/init.lua
+++ lua/kznllm/init.lua
@@ -62,13 +62,7 @@ function M.write_content_at_extmark(content, extmark_id)
 
   local lines = vim.split(content, '\n')
 
-  -- Check if there are any pending changes in the buffer
-  local undo_sequence_active = vim.bo.modified
-
-  -- Use 'undojoin' only if an undo sequence is active
-  if undo_sequence_active then
-    vim.cmd 'undojoin'
-  end
+  vim.cmd 'undojoin'
   api.nvim_buf_set_text(0, mrow, mcol, mrow, mcol, lines)
 end
 
@@ -222,6 +216,16 @@ function M.get_buffer_context(buf_id, opts)
   return buf_filetype, buf_path, buf_context
 end
 
+--- Makes a no-op change to the buffer at the specified extmark.
+--- This is used before making changes to avoid calling undojoin after undo.
+---
+---@param extmark_id integer the id of the extmark
+local function noop(extmark_id)
+  local extmark = api.nvim_buf_get_extmark_by_id(0, M.NS_ID, extmark_id, { details = false })
+  local mrow, mcol = extmark[1], extmark[2]
+  api.nvim_buf_set_text(0, mrow, mcol, mrow, mcol, {})
+end
+
 --- Working implementation of "inline" fill mode
 --- Invokes an LLM via a supported API spec defined by
 ---
@@ -279,6 +283,8 @@ function M.invoke_llm(make_data_fn, make_curl_args_fn, make_job_fn, opts)
     end
 
     local args = make_curl_args_fn(data, opts)
+
+    noop(stream_end_extmark_id)
 
     active_job = make_job_fn(args, function(content)
       M.write_content_at_extmark(content, stream_end_extmark_id)

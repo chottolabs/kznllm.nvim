2306422df522c637457943fd03e187720739981d
chottolabs <171991982+chottolabs@users.noreply.github.com>
2024-09-11 05:04:44 -0400

    fix off by one error when setting extmark

--- lua/kznllm/presets.lua
+++ lua/kznllm/presets.lua
@@ -237,7 +237,7 @@ function M.invoke_llm(make_data_fn, make_curl_args_fn, make_job_fn, opts)
       opts.debug_fn(data, M.NS_ID, stream_end_extmark_id, opts)
     else
       local _, crow, ccol = unpack(vim.fn.getpos '.')
-      stream_end_extmark_id = api.nvim_buf_set_extmark(M.BUFFER_STATE.ORIGIN, M.NS_ID, crow - 1, ccol, { strict = false })
+      stream_end_extmark_id = api.nvim_buf_set_extmark(M.BUFFER_STATE.ORIGIN, M.NS_ID, crow - 1, ccol - 1, { strict = false })
     end
 
     local args = make_curl_args_fn(data, opts)
